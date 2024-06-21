0c192032061ab4d4c224a995e3ea24310509eaff
ZaharChernenko <zaharchernenko35@gmail.com>
2024-06-22 01:09:18 +0300

    clang-format brew

--- Posix/cpp.py
+++ Posix/cpp.py
@@ -8,8 +8,11 @@ from Posix.posixTools import copyFile
 
 def installCppTools(package_manager: PackageManagers):
     print("installing cpp tools")
-    clang_format_dict = {PackageManagers.APT: "sudo apt install -y clang-tools-extra",
-                         PackageManagers.DNF: "sudo dnf install -y clang-tools-extra"}
+    clang_format_dict = {
+        PackageManagers.APT: "sudo apt install -y clang-tools-extra",
+        PackageManagers.DNF: "sudo dnf install -y clang-tools-extra",
+        PackageManagers.BREW: "brew install clang-format"
+    }
     if os.system(clang_format_dict[package_manager]) != 0:
         raise CppToolsInstallationFailed
     successPrint("cpp tools installed")
